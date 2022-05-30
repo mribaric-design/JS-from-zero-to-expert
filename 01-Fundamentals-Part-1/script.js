41b562ab478650cce40f2c7cdf7d86de3d69647e
Marko Ribarić <mribaric.riba@gmail.com>
2022-05-31 00:04:07 +0200

    01 Fundamentals
    022 Equality Operators 0:00

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -234,7 +234,7 @@ if(birthYear < 2000) {
 console.log(century);
 */
 
-
+/*
 // 019 Coding chalange #2
 
 const massMark = 78;
@@ -249,4 +249,60 @@ if(BMIMark > BMIJohn) {
     console.log(`Mark's BMI (${BMIMark}) is greater than John's BMI (${BMIJohn})`)
 } else {
     console.log(`Mark's BMI (${BMIMark}) is smaller than John's BMI (${BMIJohn})`)
+}
+*/
+
+
+/*
+// 020 Type Conversion an Coercion
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+
+console.log('I am ' + 23 + ' years old'); // + operator converst number to string 
+console.log('23' - '10' - 3); // - operator converst string to number 
+console.log('23' * '2'); // * operator convert string to number
+console.log('23' / '2'); // / operator convert string to number
+console.log('23' > '18'); // > operator convert string to number
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+*/
+
+
+
+// 021 Truthy and falsy values
+
+// 5 falsy values - 0, '', undefined, null, NaN
+
+console.log(Boolean(''));
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+
+const money = 0;
+
+if(money) {
+    console.log("Don't spend it all")
+} else {
+    console.log('You should get a job!')
+}
+
+let height;
+if(height) {
+    console.log('YAY! Height is defined!');
+} else {
+    console.log('Height is undefined!');
 }
